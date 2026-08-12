a495afb8b15cc384cc0a8eae69b36099edd6105b
unknown <dbm.laraveldeveloper5@gmail.com>
2026-08-12 12:20:54 +0530

    push

--- Modules/Import/Jobs/ProcessInventoryImportJob.php
+++ Modules/Import/Jobs/ProcessInventoryImportJob.php
@@ -29,7 +29,7 @@ class ProcessInventoryImportJob implements ShouldQueue
     protected string $excelPath;
     protected string $pptPath;
     public $tries = 3;
-    public $timeout = 900;
+    public $timeout = 900;  
 
     public $uniqueFor = 3600;
 
